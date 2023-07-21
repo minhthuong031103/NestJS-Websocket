d37487e8270c119acad9988715f94cbb03dd64f4
minhthuong031103 <nominh67@gmail.com>
2023-07-21 08:47:58 +0700

    init

---
+++ src/gateway/gateway.ts
@@ -0,0 +1,10 @@
+import {MessageBody, SubscribeMessage, WebSocketGateway} from '@nestjs/websockets';
+
+
+@WebSocketGateway()
+export class MyGateway{
+@SubscribeMessage('newMessage')
+onNewMessage(@MessageBody() body:any){
+    console.log(body)
+}
+}
